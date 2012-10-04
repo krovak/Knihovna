608f017d399ff008383e00080fdf22672752284e
mysicelesni <mysicelesni@seznam.cz>
2012-10-04 15:12:04 +0200

    Registrace modulu

---
+++ app/code/local/Knihovna/MysicelesniTest/sql/knihovna_mysicelesnitest_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ja
+ * Date: 4.10.12
+ * Time: 14:26
+ * To change this template use File | Settings | File Templates.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
