1c253304e6384b97ca3db080988f57aa28ce15d3
Jakub Starý <info@jakubstary.cz>
2012-10-04 15:07:58 +0200

    Pridani menu item

---
+++ app/code/local/Knihovna/KubaTest/sql/knihovna_kubatest_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: admin
+ * Date: 4.10.12
+ * Time: 14:24
+ * To change this template use File | Settings | File Templates.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
