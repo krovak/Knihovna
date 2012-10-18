1854f7f904c600d904976141ba3dcb277c3ad01b
Premek Sumpela <supr32@gmail.com>
2012-10-18 15:10:08 +0200

    muj modul :-)

---
+++ app/code/local/Knihovna/SuprTest/sql/knihovna_suprtest_setup/install-0.1.0.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author: Premek Sumpela <supr32@gmail.com>
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('suprtest/test'))
+    ->addColumn('test_id',Varien_Db_Ddl_Table::TYPE_SMALLINT,null,array(
+    'identity' => true,
+    'nullable' => false,
+    'primary' => true,
+),'Test ID')
+    ->addColumn('title',Varien_Db_Ddl_Table::TYPE_VARCHAR,255,array(
+    'nullable' => false,
+),'Nazev')
+    ->addColumn('creation_time',Varien_Db_Ddl_Table::TYPE_TIMESTAMP,null,array(),'Creation Time')
+    ->setComment('Testovací Tabulka');
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
