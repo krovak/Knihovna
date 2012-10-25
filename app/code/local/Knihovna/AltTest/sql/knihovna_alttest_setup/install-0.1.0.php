ee76c948057cbfc1a576e95c2628ea3afa09c603
altpetr <alt.p@seznam.cz>
2012-10-25 14:47:50 +0200

    tabulka

---
+++ app/code/local/Knihovna/AltTest/sql/knihovna_alttest_setup/install-0.1.0.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: altpetr
+ * Date: 4.10.12
+ * Time: 14:23
+ * To change this template use File | Settings | File Templates.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+$table = $installer->getConnection() //pripojime se k databazi
+    ->newTable($installer->$getTable('knihovna_alttest/knihovna_alttest')) //resource knihovna_alttest, entita knihovna_alttest
+    ->addColumn('entity_id',Varien_Db_Ddl_Table::TYPE_SMALLINT,null,array(
+    'identity' => true,
+    'nullable' => false,
+    'primary'  => true,
+),'Entity_id')
+    ->addColumn('jmeno',Varien_Db_Dbl_Table::TYPE_VARCHAR,50,array(
+    'nullable' => false //jmeno musi byt vyplneno
+), 'Jméno')
+    ->addColumn('prijmeni',Varien_Db_Dbl_Table::TYPE_VARCHAR,50,array(
+    'nullable' => false //prijmeni musi byt take vyplneno
+),'Příjmení');
+
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
