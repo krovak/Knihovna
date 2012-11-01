6183c1d3273708fd7d47c3f50af1a84886abe458
Michal Moc <michal.moc@gmail.com>
2012-11-01 14:37:17 +0100

    Přidány sloupce tabulky

--- app/code/local/Knihovna/MichalTest/Block/Adminhtml/MichalTest/Grid.php
+++ app/code/local/Knihovna/MichalTest/Block/Adminhtml/MichalTest/Grid.php
@@ -7,10 +7,11 @@
  * www: http://www.iguru.eu
  * email: info@iguru.eu
  */
- 
+
 class Knihovna_MichalTest_Block_Adminhtml_MichalTest_Grid extends Mage_Adminhtml_Block_Widget_Grid
 {
-    public function _construct(){
+    public function _construct()
+    {
         $this->setId('michaltestGrid');
         $this->setDefaultSort('entity_id');
         $this->setDefaultDir('ASC');
@@ -18,11 +19,30 @@ class Knihovna_MichalTest_Block_Adminhtml_MichalTest_Grid extends Mage_Adminhtml
         $this->setSaveParametersInSession(true);
     }
 
-    protected function _prepareCollection(){
+    protected function _prepareCollection()
+    {
         $collection = Mage::getResourceSingleton('knihovna_michaltest/knihovna_michaltest_collection');
         $this->setCollection($collection);
         return parent::_prepareCollection();
     }
 
-
+    protected function _prepareColumns()
+    {
+        $this->addColumn('entity_id', array(
+            'header' => $this->__('ID'),
+            'width'  => '60px',
+            'index'  => 'entity_id'
+        ));
+        $this->addColumn('jmeno', array(
+            'header' => $this->__('Jméno'),
+            'align'  => 'left',
+            'index'  => 'jmeno'
+        ));
+        $this->addColumn('prijmeni', array(
+            'header' => $this->__('Příjmení'),
+            'align'  => 'left',
+            'index'  => 'prijmeni'
+        ));
+        return $this;
+    }
 }
