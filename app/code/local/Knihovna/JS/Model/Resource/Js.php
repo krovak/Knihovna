89e76dbcec3cefe118c6ae2290dfd712b369d67e
Jakub Starý <info@jakubstary.cz>
2012-11-15 14:31:32 +0100

    Oprava Resourcu v JS

--- app/code/local/Knihovna/JS/Model/Resource/Js.php
+++ app/code/local/Knihovna/JS/Model/Resource/Js.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-class Knihovna_JS_Model_Resource_Js extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+class Knihovna_JS_Model_Resource_Js extends Mage_Core_Model_Resource_Db_Abstract {
     protected function _construct() {
         $this->_init('js/knihovna_js','entity_id');
     }
