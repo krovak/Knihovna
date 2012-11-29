da278b52782949f7e9c37fbf8fa1705a275f8503
tichave4 <tichaveru@gmail.com>
2012-11-29 14:32:15 +0100

    oprava modulu

--- app/code/local/Knihovna/Titest/controllers/Adminhtml/TitestController.php
+++ app/code/local/Knihovna/Titest/controllers/Adminhtml/TitestController.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.*/
 class Knihovna_Titest_Adminhtml_TitestController extends Mage_Adminhtml_Controller_Action{
     public function indexAction(){
-        $this->_initAction()->_addContent($this->getLayout()->createBlock('titest/adminthtml_titest'))->renderLayout();
+        $this->_initAction()->_addContent($this->getLayout()->createBlock('titest/adminhtml_titest'))->renderLayout();
             }
     public function _initAction(){
         $this->loadLayout();
