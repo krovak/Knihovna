b1dcfa7418200ab6a511cd7bdf97397c4ffcb0ed
Michal Moc <michal.moc@gmail.com>
2012-11-29 20:56:14 +0100

    Opravena Collection

--- app/code/local/Knihovna/Mitest/Model/Resource/Mitest/Collection.php
+++ app/code/local/Knihovna/Mitest/Model/Resource/Mitest/Collection.php
@@ -9,7 +9,8 @@
  */
  
 class Knihovna_Mitest_Model_Resource_Mitest_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+
     public function _construct(){
-        $this->_init('mitest');
+        $this->_init('mitest/mitest');
     }
 }
