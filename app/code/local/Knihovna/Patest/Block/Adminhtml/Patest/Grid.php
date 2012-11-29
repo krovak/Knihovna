02e4314c1c905258ad35c396cde359e3b23c4d68
Petr Alt <alt.p@seznam.cz>
2012-11-29 14:08:19 +0100

    Grid

--- app/code/local/Knihovna/Patest/Block/Adminhtml/Patest/Grid.php
+++ app/code/local/Knihovna/Patest/Block/Adminhtml/Patest/Grid.php
@@ -8,5 +8,34 @@
  */
 
 class Knihovna_Patest_Block_Adminhtml_Patest_Grid extends Mage_Adminhtml_Block_Widget_Grid {
+    public function _construct(){
+            parent::_construct();
+            $this->setId('patest');
+            $this->setDefaultSort('entity_id');
+            $this->setDefaultDir('asc');
+            $this->setEmptyText('Žádná data');
+            $this->setSaveParametersInSession(true);
+    }
 
+    public function _prepareCollection(){
+            $collection = Mage::getModel('patest/patest')->getCollection();
+            $this->setCollection($collection);
+            return parent::_prepareCollection();
+        }
+
+    public function _prepareColumns(){
+            $this->addColumn('entity_id',array(
+                'header'=>'ID',
+                'index'=>'entity_id'
+            ));
+            $this->addColumn('jmeno',array(
+                    'header'=>'Jméno',
+                    'index'=>'jmeno'
+            ));
+            $this->addColumn('prijmeni',array(
+                    'header'=>'Příjmení',
+                    'index'=>'prijmeni'
+            ));
+        return $this;
+    }
 }
--- app/code/local/Knihovna/Patest/controllers/Adminhtml/PatestController.php
+++ app/code/local/Knihovna/Patest/controllers/Adminhtml/PatestController.php
@@ -8,5 +8,12 @@
  */
 
 class Knihovna_Patest_Adminhtml_PatestController extends Mage_Adminhtml_Controller_Action {
+    public function indexAction(){
+        $this->_initAction()->_addContent($this->getLayout()->createBlock('patest/adminhtml_patest'))->renderLayout();
+    }
 
+    public function _initAction(){
+        $this->loadLayout();
+        return $this;
+    }
 }
