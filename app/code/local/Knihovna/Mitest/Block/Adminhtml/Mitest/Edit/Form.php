13f932f3c2f505a218ccce72f49dee6f9612e636
Michal Moc <michal.moc@gmail.com>
2012-11-29 21:03:39 +0100

    drobné změny a přidán druhý parametr přijmení, který je povinný

--- app/code/local/Knihovna/Mitest/Block/Adminhtml/Mitest/Edit/Form.php
+++ app/code/local/Knihovna/Mitest/Block/Adminhtml/Mitest/Edit/Form.php
@@ -7,32 +7,42 @@
  * www: http://www.iguru.eu
  * email: info@iguru.eu
  */
- 
-class Knihovna_Mitest_Block_Adminhtml_Mitest_Edit_Form
-  extends Mage_Adminhtml_Block_Widget_Form {
 
-    public function _construct(){
-         parent::_construct();
-         $this->setId('mitestForm');
-         $this->setTitle('Přidat záznam');
-     }
-     public function _prepareForm(){
-         $form = new Varien_Data_Form(array(
-             'id'=>'mitestForm',
-             'method'=>'Post'
-         ));
-         $f = $form->addFieldset('mitest',array(
-             'legend'=>'Přidat autora',
-             'class'=>'fieldset-wide'
-         ));
-         $f->addField('jmeno','text',array(
-             'name'=>'jmeno',
-             'label'=>'Jméno',
-             'required'=>true
-         ));
-         $form->setUseContainer(true);
-         $form->setAction($this->getUrl('*/*/save'));
-         $this->setForm($form);
-         return parent::_prepareForm();
-     }
+class Knihovna_Mitest_Block_Adminhtml_Mitest_Edit_Form
+    extends Mage_Adminhtml_Block_Widget_Form
+{
+
+    public function _construct()
+    {
+        parent::_construct();
+        $this->setId('editForm');
+        $this->setTitle('Přidat záznam');
+    }
+
+    public function _prepareForm()
+    {
+        $form = new Varien_Data_Form(array(
+            'id'     => 'edit_form',
+            'method' => 'post'
+        ));
+        $f    = $form->addFieldset('mitest', array(
+            'legend' => 'Přidat autora',
+            'class'  => 'fieldset-wide'
+        ));
+        $f->addField('jmeno', 'text', array(
+            'name'     => 'jmeno',
+            'label'    => 'Jméno',
+            'required' => true
+        ));
+        $f->addField('prijmeni', 'text', array(
+            'name'     => 'prijmeni',
+            'label'    => 'Přijmení',
+            'required' => true
+        ));
+
+        $form->setUseContainer(true);
+        $form->setAction($this->getUrl('*/*/save'));
+        $this->setForm($form);
+        return parent::_prepareForm();
+    }
 }
