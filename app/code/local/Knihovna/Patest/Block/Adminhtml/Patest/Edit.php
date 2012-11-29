81bb60aa6783ed6002f189167392ac7ec356b38c
Petr Alt <alt.p@seznam.cz>
2012-11-29 15:27:18 +0100

    Edit, Form, druhý pokus

---
+++ app/code/local/Knihovna/Patest/Block/Adminhtml/Patest/Edit.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: altpetr
+ * Date: 29.11.12
+ * Time: 14:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Knihovna_Patest_Block_Adminhtml_Patest_Edit extends Mage_Adminhtml_Block_Widget_Form_Container {
+
+
+    public function _construct(){
+    $this->_objectId='entity_id';
+    $this->_controller='adminhtml_patest';
+    $this->_blockGroup='patest';
+    parent::_construct();
+}
+    public function getHeaderText(){
+    return 'Přidat záznam';
+}
+}
