5e82616b5783537f362946c4b8488cc16afeb630
Jakub Starý <info@jakubstary.cz>
2012-12-06 14:11:06 +0100

    Úprava dodělání JS

--- app/code/local/Knihovna/KubaTest/Model/Resource/Tabulka/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Knihovna_KubaTest_Model_Resource_KubaTest_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
-{
-    public function _construct(){
-        $this->_init('knihovna_kubatest/knihovna_kubatest');
-    }
-}
