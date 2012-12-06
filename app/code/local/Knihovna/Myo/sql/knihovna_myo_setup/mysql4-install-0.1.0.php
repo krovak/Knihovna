08a65569b366cae5f7b98e042e34942c4e5fd53f
Lucie Mikešová <Lucie7C7@gmail.com>
2012-12-06 14:49:41 +0100

    modul Myo

---
+++ app/code/local/Knihovna/Myo/sql/knihovna_myo_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Myodrag
+ * Date: 6.12.12
+ * Time: 14:47
+ * To change this template use File | Settings | File Templates.
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
