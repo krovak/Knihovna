aedcb8619ebe098c0db906978b96e7aa9d98861d
Vít Jašek <spike.99@seznam.cz>
2012-12-06 14:28:44 +0100

    smazany testy

--- app/code/local/Knihovna/VitaTest/Model/Resource/VitaTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Knihovna_VitaTest_Model_Resource_VitaTest extends Mage_Adminhtml_Block_Widget_Grid_Container
-{
-    public function _construct(){
-        $this->_init('knihovna_vitavest/knihovna_vitatest','entitiy_id');
-    }
-}
