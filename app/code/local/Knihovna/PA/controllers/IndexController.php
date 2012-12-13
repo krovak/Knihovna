1f0f4a4f4c284e30aa721375b1b50220a7564ab1
Petr Alt <alt.p@seznam.cz>
2012-12-13 14:37:26 +0100

    Smazání starého testu

--- app/code/local/Knihovna/PA/controllers/IndexController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Knihovna_PA_IndexController extends Mage_Core_Controller_Front_Action {
-    public function indexAction(){
-                $collection = Mage::getModel('pa/pa')->getCollection();
-                var_dump($collection);
-    }
-}
