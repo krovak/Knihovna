57efa904d56e0d1f975c9cfc5c63d08c89d467d5
Vít Jašek <spike.99@seznam.cz>
2012-12-13 15:05:35 +0100

    id

--- app/code/local/Knihovna/VJ/Block/Adminhtml/Vj/Edit.php
+++ app/code/local/Knihovna/VJ/Block/Adminhtml/Vj/Edit.php
@@ -10,7 +10,7 @@
  
 class Knihovna_VJ_Block_Adminhtml_Vj_Edit extends Mage_Adminhtml_Block_Widget_Form_Container {
     public function _construct(){
-        $this->objectId='entity_id';
+        $this->_objectId='id';
         $this->_controller='adminhtml_vj';
         $this->_blockGroup='vj';
         parent::_construct();
