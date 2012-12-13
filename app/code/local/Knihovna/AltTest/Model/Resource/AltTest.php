e3233dc683d6ec9959ae106a576864f7a0618ed3
Petr Alt <alt.p@seznam.cz>
2012-12-13 13:45:26 +0100

    Smazání starých testů

--- app/code/local/Knihovna/AltTest/Model/Resource/AltTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Knihovna_AltTest_Model_Resource_AltTest extends Mage_Core_Model_Resource_Db_Abstract
-{
-    public function _construct(){
-               $this->_init('knihovna_alttest/knihovna_alttest','entitiy_id');
-    }
-}
