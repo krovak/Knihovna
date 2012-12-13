0c5bb0da52657534ab08af5acc345a79441f7c5b
tichave4 <tichaveru@gmail.com>
2012-12-13 14:14:33 +0100

    pridani modulu ctenar

--- app/code/local/Knihovna/Titest/Block/Adminhtml/Titest/Edit/Form.php
+++ app/code/local/Knihovna/Titest/Block/Adminhtml/Titest/Edit/Form.php
@@ -6,41 +6,47 @@
  * Time: 15:08
  * To change this template use File | Settings | File Templates.
  */
-class Knihovna_Titest_Block_Adminhtml_Titest_Edit_Form extends Mage_Adminhtml_Block_Widget_Form{
-    public function _construct(){
+class Knihovna_Titest_Block_Adminhtml_Titest_Edit_Form extends Mage_Adminhtml_Block_Widget_Form
+{
+    public function _construct()
+    {
         parent::_construct();
         $this->setId('editForm');
         $this->setTitle('Pridat zaznam');
     }
-    public function _prepareForm(){
+
+    public function _prepareForm()
+    {
         $ctenar = Mage::registry('titest');
 
         $form = new Varien_Data_Form(array(
-            'id'=>'edit_form',
-            'method'=>'Post'
+            'id' => 'edit_form',
+            'method' => 'Post'
         ));
-        $f    = $form->addFieldset('titest', array(
+        $f = $form->addFieldset('titest', array(
             'legend' => 'Přidat čtenáře',
-            'class'  => 'fieldset-wide'
+            'class' => 'fieldset-wide'
         ));
         if ($ctenar->getId()) {
             $f->addField('entity_id', 'hidden', array(
                 'name' => 'entity_id'
             ));
+        }
 
+            $f->addField('jmeno', 'text', array(
+                'name' => 'jmeno',
+                'label' => 'Jméno',
+                'required' => true
+            ));
+            $f->addField('prijmeni', 'text', array(
+                'name' => 'prijmeni',
+                'label' => 'Přijmení',
+                'required' => true
+            ));
+            $form->setUseContainer(true);
+            $form->setAction($this->getUrl('*/*/save'));
+            $this->setForm($form);
+            return parent::_prepareForm();
 
-        $f->addField('jmeno', 'text', array(
-            'name'     => 'jmeno',
-            'label'    => 'Jméno',
-            'required' => true
-        ));
-        $f->addField('prijmeni', 'text', array(
-            'name'     => 'prijmeni',
-            'label'    => 'Přijmení',
-            'required' => true
-        ));
-        $form->setUseContainer(true);
-        $form->setAction($this->getUrl('*/*/save'));
-        $this->setForm($form);
-        return parent::_prepareForm();
-    }}}
+    }
+}
