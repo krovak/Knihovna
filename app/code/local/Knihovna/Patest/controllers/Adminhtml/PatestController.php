ed45d1a5f706eee91e6ee42910a35937008ac224
Petr <alt.p@seznam.cz>
2012-12-20 00:43:32 +0100

    Revert "Revert "Revert "Přidání modulu Oddělení"""
    This reverts commit 463663cac65675fab2439c1062223de84b5db928.

--- app/code/local/Knihovna/Patest/controllers/Adminhtml/PatestController.php
+++ app/code/local/Knihovna/Patest/controllers/Adminhtml/PatestController.php
@@ -43,8 +43,11 @@ class Knihovna_Patest_Adminhtml_PatestController extends Mage_Adminhtml_Controll
     {
 
         $this->loadLayout();
-        $oddeleni=$this->_initEdit('id');
-        $this->_addContent($this->getLayout()->createBlock('patest/adminhtml_patest_edit')->setEditMode((bool)$this->getRequest()->getParam('entity_id')));
+        $autor=$this->_initEdit('id');
+        $this->_addContent($this->getLayout()
+            ->createBlock('patest/adminhtml_patest_edit')
+            ->setEditMode((bool)$this->getRequest()
+            ->getParam('entity_id')));
         $this->renderLayout();
     }
 
