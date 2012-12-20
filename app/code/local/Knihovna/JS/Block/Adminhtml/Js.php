aaafe088ed16bbcae24555ba6231507feda53c28
Jakub Starý <info@jakubstary.cz>
2012-12-20 14:08:44 +0100

    select9

--- app/code/local/Knihovna/JS/Block/Adminhtml/Js.php
+++ app/code/local/Knihovna/JS/Block/Adminhtml/Js.php
@@ -11,6 +11,14 @@ class Knihovna_JS_Block_Adminhtml_Js extends Mage_Adminhtml_Block_Widget_Grid_Co
     public function _construct() {
         $this->_controller='adminhtml_js';
         $this->_blockGroup='js';
+        $knihy = array();
+        $model = Mage::getModel('vj/vj');
+        $collection = $model->getCollection();
+        foreach($collection as $item){
+            $knihy[$item->getData('entity_id')][' idecko '] = $item->getData('entity_id');
+            $knihy[$item->getData('entity_id')][' nazev '] = $item->getData('nazev');
+        }
+        print_r($knihy);
         parent::_construct();
     }
     public function getHeaderText() {
