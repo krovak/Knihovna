ca54ef5a70d55c3efcd3840f2e9cb01614168f18
Vít Jašek <spike.99@seznam.cz>
2013-01-17 12:45:56 +0100

    oprava

--- app/code/local/Knihovna/Zanr/Block/Adminhtml/Zanr/Renderer/Zanr.php
+++ app/code/local/Knihovna/Zanr/Block/Adminhtml/Zanr/Renderer/Zanr.php
@@ -13,6 +13,6 @@ class Knihovna_Zanr_Block_Adminhtml_Zanr_Renderer_Zanr extends Mage_Adminhtml_Bl
         {
             $value = $row->getData($this->getColumn()->getIndex());
             $zanr = Mage::getModel('zanr/zanr')->load($value);
-            return ($zanr->getName());
+            return ($zanr->getNazev());
         }
 }
