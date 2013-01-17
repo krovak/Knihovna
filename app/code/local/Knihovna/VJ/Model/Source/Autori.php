fe00b1e6fa30e8128ec011b3fe4ff964d07b54b1
Michal Moc <michal.moc@gmail.com>
2013-01-17 10:29:24 +0100

    Provedený refaktoring (přejmenování modulu z mitest na autor)

--- app/code/local/Knihovna/VJ/Model/Source/Autori.php
+++ app/code/local/Knihovna/VJ/Model/Source/Autori.php
@@ -14,7 +14,7 @@ class Knihovna_VJ_Model_Source_Autori
     {
         $this->autori[0]['label'] = '-- Vyberte --';
         $this->autori[0]['value'] = '';
-        $autori                   = Mage::getModel('mitest/mitest')->getCollection();
+        $autori                   = Mage::getModel('autor/autor')->getCollection();
         /**@var $autor Knihovna_VJ_Model_Autori */
         foreach ($autori as $autor) {
             $id             = $autor->getId();
