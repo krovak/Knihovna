0ffe87ff591bf7f3129396c41875c640f0a3f67e
verča <tichaveru@gmail.com>
2013-01-24 12:46:30 +0100

    číslo průkazu4

--- app/code/local/Knihovna/Titest/Model/Titest.php
+++ app/code/local/Knihovna/Titest/Model/Titest.php
@@ -17,7 +17,7 @@ class Knihovna_Titest_Model_Titest extends Mage_Core_Model_Abstract{
         $delka=Mage::getStoreConfig('prukazka/cislo/delka');
         $vypln=Mage::getStoreConfig('prukazka/cislo/vypln');
         $posledni_cislo=Mage::getModel('titest/titest')->getCollection()->getAttributeToSort('entity_id','desc')->limit(1);
-        var_dump($prefix);
+        var_dump($posledni_cislo);
         die;
         $cp =  $prefix . str_pad(($posledni_cislo->getId())+1,$delka-strlen($prefix),$vypln,STR_PAD_LEFT);
 
