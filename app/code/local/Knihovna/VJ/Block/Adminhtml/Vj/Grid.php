1a14dd18239be9f675a42462a8504d60c5726de5
Vít Jašek <spike.99@seznam.cz>
2013-01-24 11:04:15 +0100

    pokus

--- app/code/local/Knihovna/VJ/Block/Adminhtml/Vj/Grid.php
+++ app/code/local/Knihovna/VJ/Block/Adminhtml/Vj/Grid.php
@@ -25,7 +25,8 @@ class Knihovna_VJ_Block_Adminhtml_Vj_Grid extends Mage_Adminhtml_Block_Widget_Gr
 
         $collection = Mage::getModel('vj/vj')->getCollection();
         $autori_tabulka = Mage::getSingleton('core/resource')->getTableName('autor/knihovna_autor');
-        $collection->getSelect()->join(array('mi' => $autori_tabulka), 'mi.entity_id=main_table.autor', array('autor' => 'jmeno','autor'=>'prijmeni'));
+        $collection->getSelect()->join(array('mi' => $autori_tabulka), 'mi.entity_id=main_table.autor', array('autor' => 'jmeno'));
+        $collection->getSelect()->join(array('mi2' => $autori_tabulka), 'mi0.entity_id=main_table.autor', array('autor' => 'prijmeni'));
         $zanr_tabulka = Mage::getSingleton('core/resource')->getTableName('zanr/knihovna_zanr');
         $collection->getSelect()->join(array('vt' => $zanr_tabulka), 'vt.entity_id=main_table.zanr', array('zanr' => 'nazev'));
         $this->setCollection($collection);
