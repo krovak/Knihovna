930c80d106538f4124d7c69c247f9ea5078aadc5
verča <tichaveru@gmail.com>
2013-01-31 15:34:56 +0100

    pridana funkce validate

--- app/code/local/Knihovna/Ctenar/controllers/IndexController.php
+++ app/code/local/Knihovna/Ctenar/controllers/IndexController.php
@@ -8,6 +8,6 @@ class Knihovna_Ctenar_IndexController extends Mage_Core_Controller_Front_Action
     public function testAction(){
         $cp = $this->getRequest()->getParam('cp');
         $pw = $this->getRequest()->getParam('pw');
-        Mage::getModel('ctenar/ctenar')->test($cp,$pw);
+        Mage::getModel('ctenar/ctenar')->validate($cp,$pw);
     }
 }
