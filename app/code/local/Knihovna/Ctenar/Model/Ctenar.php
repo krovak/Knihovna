f298c1a14983812dc4a3020ed81650a3d46cbef1
Veru <tichaveru@gmail.com>
2013-02-07 14:58:15 +0100

    login

--- app/code/local/Knihovna/Ctenar/Model/Ctenar.php
+++ app/code/local/Knihovna/Ctenar/Model/Ctenar.php
@@ -17,18 +17,24 @@ class Knihovna_Ctenar_Model_Ctenar extends Mage_Core_Model_Abstract
     public function getCisloprukazky()
     {
         $prefix = Mage::getStoreConfig('prukazka/cislo/prefix');
-        $delka  = Mage::getStoreConfig('prukazka/cislo/delka');
-        $vypln  = Mage::getStoreConfig('prukazka/cislo/vypln');
+        $delka = Mage::getStoreConfig('prukazka/cislo/delka');
+        $vypln = Mage::getStoreConfig('prukazka/cislo/vypln');
         /* @var $posledni_cislo Knihovna_Ctenar_Model_Resource_Ctenar_Collection */
         $posledni_cislo = Mage::getModel('ctenar/ctenar')->getCollection()->getLastItem();
 
         $cp = $prefix . str_pad(($posledni_cislo->getId()) + 1, $delka - strlen($prefix), $vypln, STR_PAD_LEFT);
         return $cp;
     }
-    public function validate($cp,$heslo){
-       $db = Mage::getModel('ctenar/ctenar')->getCollection()->addFieldToFilter('cislo_prukazu',array('eq'=>$cp));
-       var_dump($db->getData());
-        die;
+
+    public function validate($cp, $heslo)
+    {
+        $db = Mage::getModel('ctenar/ctenar')->getCollection()->addFieldToFilter('cislo_prukazu', array('eq' => $cp));
+        $data = $db->getData();
+        if ($data['entity_id']) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- app/code/local/Knihovna/Ctenar/controllers/LoginController.php
+++ app/code/local/Knihovna/Ctenar/controllers/LoginController.php
@@ -10,10 +10,25 @@ class Knihovna_Ctenar_LoginController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
+        $name = $this->getRequest()->getPost('name');
+        $heslo = $this->getRequest()->getPost('password');
+        if (Mage::getModel('ctenar/ctenar')->validate($name, $heslo)) {
+            $this->_redirect('*/*/logged');
+        } else {
+            $this->loadLayout();
+            $this->getLayout()->getBlock('content')->append($this->getLayout()->createBlock('ctenar/login'));
+            $this->renderLayout();
+        }
+    }
 
-        $this->loadLayout();
-        $this->getLayout()->getBlock('content')->append($this->getLayout()->createBlock('ctenar/login'));
-        $this->renderLayout();
+    public function logoutAction()
+    {
+
+    }
+
+    public function loggedAction()
+    {
+echo('Gratuluji podařilo se Vám přihlásit');
     }
 
 }
