fe6b5c6fa4213acf05303cb2d5bc2221c458e269
altpetr <alt.p@seznam.cz>
2013-02-14 12:36:04 +0100

    Pokus o opravu modulu Knihovna

--- app/code/local/Knihovna/Knihovna/controllers/Adminhtml/KnihovnaController.php
+++ app/code/local/Knihovna/Knihovna/controllers/Adminhtml/KnihovnaController.php
@@ -14,6 +14,7 @@ public function indexAction(){
 
 protected function _initEdit($idFileName='entity_id'){
     $id=$this->getRequest()->getParam($idFileName);
+    var_dump($this);
     $model=Mage::getModel('knihovna/knihovna');
     if($id){
         $model->load($id);
