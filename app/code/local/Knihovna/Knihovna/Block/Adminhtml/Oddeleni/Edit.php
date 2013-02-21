60aa3c3328f00936d3f797d7fa9caf682b5e7fb6
Petr <alt.p@seznam.cz>
2013-02-21 13:32:15 +0100

    Už to funguje.

--- app/code/local/Knihovna/Knihovna/Block/Adminhtml/Oddeleni/Edit.php
+++ app/code/local/Knihovna/Knihovna/Block/Adminhtml/Oddeleni/Edit.php
@@ -4,9 +4,7 @@
  * User: altpetr
  * Date: 29.11.12
  * Time: 14:59
- * To change this template use File | Settings | File Templates.
  */
-
 class Knihovna_Knihovna_Block_Adminhtml_Oddeleni_Edit extends Mage_Adminhtml_Block_Widget_Form_Container {
 
 
