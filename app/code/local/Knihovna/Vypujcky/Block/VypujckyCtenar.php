5ad535046ffe43076fa57cb68b88426fb724b946
Jakub Starý <info@jakubstary.cz>
2013-02-28 11:47:22 +0100

    Úprava templatu výpůjček

--- app/code/local/Knihovna/Vypujcky/Block/VypujckyCtenar.php
+++ app/code/local/Knihovna/Vypujcky/Block/VypujckyCtenar.php
@@ -15,7 +15,7 @@ class Knihovna_Vypujcky_Block_VypujckyCtenar extends Mage_Core_Block_Template{
     public function getCtenarVypujcky($id_reader)
     {
         $readers_books = Mage::getModel('vypujcky/vypujcky')->getCollection()
-            ->addFieldToFilter('reader', $id_reader);
+            ->addFieldToFilter('reader', array('eq'=>$id_reader));
         return $readers_books;
     }
 }
