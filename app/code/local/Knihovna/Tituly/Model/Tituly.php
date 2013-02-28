30dc7c74ab91a12ea4df8997e65a2acc63474af7
Vita <spike.99@seznam.cz>
2013-02-28 12:09:25 +0100

    getName

--- app/code/local/Knihovna/Tituly/Model/Tituly.php
+++ app/code/local/Knihovna/Tituly/Model/Tituly.php
@@ -8,4 +8,10 @@ class Knihovna_Tituly_Model_Tituly extends Mage_Core_Model_Abstract
          $this->_init('tituly/tituly');
     }
 
+    public function getName($id)
+    {
+        $nazev= $this ->getCollection() -> addFieldToFilter ('entity_id', array('eq' =>$id))-> getNazev();
+        return $nazev;
+    }
+
 }
