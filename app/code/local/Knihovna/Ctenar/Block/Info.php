b4c974d64f188f65b6e951562ec738835af96db1
Veru <tichaveru@gmail.com>
2013-02-28 10:33:59 +0100

    login

--- app/code/local/Knihovna/Ctenar/Block/Info.php
+++ app/code/local/Knihovna/Ctenar/Block/Info.php
@@ -1,8 +1,8 @@
 <?php
 class Knihovna_Ctenar_Block_Info extends Mage_Core_Block_Template {
     public function __construct(){
-        parent::__construct();
-        $this->setTemplate('knihovna/ctenar/info.phtml');
+        //parent::__construct();
+        //$this->setTemplate('knihovna/ctenar/info.phtml');
 
     }
 }
