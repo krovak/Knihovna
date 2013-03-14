5b3fd1414893fb9a7fd61031c2a90705e2b4a5d1
Jakub Starý <info@jakubstary.cz>
2013-03-14 11:26:37 +0100

    Načtení autora z GB

--- app/code/local/Knihovna/Tituly/Model/Import.php
+++ app/code/local/Knihovna/Tituly/Model/Import.php
@@ -76,7 +76,7 @@ class Knihovna_Tituly_Model_Import extends Mage_Core_Model_Abstract
         $child = $xmlObj->entry->children($namespaces['dc']); //vyhledam vsechny potomky s dc
 
         //TODO předělat posílání autora jako pole s ID autorů
-        $info = array('autor'     => (string)$child->creator.implode(","),
+        $info = array('autor'     => (string)$child->creator,
                       'nazev'     => (string)$child->title,
                       'rokVydani' => (string)$child->date,
                       'format'    => $child->format,
