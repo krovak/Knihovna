318f111b26baeab02140b5f9d254d9ee21f4b708
Jakub Starý <info@jakubstary.cz>
2013-03-14 10:45:05 +0100

    Zkouška načtení autora :-)

--- app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly/Edit/Form.php
+++ app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly/Edit/Form.php
@@ -81,6 +81,15 @@ class Knihovna_Tituly_Block_Adminhtml_Tituly_Edit_Form extends Mage_Adminhtml_Bl
                   var odpoved=JSON.parse(transport.responseText);
                     $('nazev').value = odpoved.nazev;
                     $('rok_vydani').value = odpoved.rokVydani;
+                    $('autor option').each(
+                    function() {
+                        var name = $(this).text();
+                        var google_name = odpoved.autor;
+                        if(name[1] == google_name[2] && name[2] == google_name[1]) {
+                            $('autor option:selected').removeAttr('selected');
+                            $(this).selected = 'selected';
+                        }
+                    });
                     $('pocet_stranek').value = parseInt(odpoved.format[0]);
                     $('tituly').insert({top:new Element('img',{src:odpoved.obrazek,style:'float:right'})});
             }
