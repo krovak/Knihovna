c752c89aed3d193d0db99fece78e8ea025c601b5
Vita <spike.99@seznam.cz>
2013-03-21 13:42:12 +0100

    Uprava validace a mazani obrazku

--- app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly/Edit/Form.php
+++ app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly/Edit/Form.php
@@ -91,7 +91,7 @@ class Knihovna_Tituly_Block_Adminhtml_Tituly_Edit_Form extends Mage_Adminhtml_Bl
                     $('rok_vydani').value = odpoved.rokVydani;
                     $('autor').value = odpoved.autor;
                     $('pocet_stranek').value = parseInt(odpoved.format[0]);
-                    $('imagecover').update(imagecover);
+                    $('tituly').insert({top :imagecover.src});
                 }
                 });
             }
