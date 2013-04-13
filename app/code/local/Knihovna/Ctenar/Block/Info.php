a8519e8c494544d6c87db3e9a4b8a739982e8c5b
Petr <alt.p@seznam.cz>
2013-04-13 16:55:06 +0200

    Test.

--- app/code/local/Knihovna/Ctenar/Block/Info.php
+++ app/code/local/Knihovna/Ctenar/Block/Info.php
@@ -11,60 +11,18 @@ class Knihovna_Ctenar_Block_Info extends Mage_Core_Block_Template {
 
 
 
-               $form = new Varien_Data_Form();
-               $this->setForm($form);
-               $f    = $form->addFieldset(
-                   'ctenar-nepovinne', array(
-                       'legend' => 'Přidání nepovinných údajů',
-                       'class'  => 'fieldset-wide'
-                   )
-               );
-               $f->addField(
-                   'mesto', 'text', array(
-                       'name'     => 'mesto',
-                       'label'    => 'Město',
-                       'required' => false
-                   )
-               );
-               $f->addField(
-                   'psc', 'text', array(
-                       'name'     => 'psc',
-                       'label'    => 'PSČ',
-                       'required' => false
-                   )
-               );
-               $f->addField(
-                   'heslo', 'password', array(
-                       'name'     => 'heslo',
-                       'label'    => 'Heslo',
-                       'required' => false
-                   )
-               );
-               $f->addField(
-                   'potvrzeni_hesla', 'password', array(
-                       'name'     => 'potvrzeni_hesla',
-                       'label'    => 'Potvrzení hesla',
-                       'required' => false
-                   )
-               );
-               $f->addField(
-                   'email', 'text', array(
-                       'name'     => 'email',
-                       'label'    => 'Email',
-                       'required' => false
-                   )
-               );
-               $f->addField(
-                   'telefonni_cislo', 'text', array(
-                       'name'     => 'telefonni_cislo',
-                       'label'    => 'Telefonní číslo',
-                       'required' => false
-                   )
-               );
-                   $form->setValues($ctenar->getData());
-//            $form->setUseContainer(true);
+           $form = new Varien_Data_Form(array(
+               'id'     => 'edit_form',
+               'method' => 'Post'
+           ));
+           $f    = $form->addFieldset(
+               'ctenar', array(
+                   'legend' => 'Přidat čtenáře',
+                   'class'  => 'fieldset-wide'
+               )
+           );
 
-           return $ctenar;
+           //return $ctenar; //pozdeji odkomentuju
 
        }
 
