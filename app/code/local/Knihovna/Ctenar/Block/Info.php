597b20a93d85d8aed3e6d41d7a52b5945b5e86ed
Petr <alt.p@seznam.cz>
2013-04-13 22:24:31 +0200

    Pokus o opravu chyby.

--- app/code/local/Knihovna/Ctenar/Block/Info.php
+++ app/code/local/Knihovna/Ctenar/Block/Info.php
@@ -3,7 +3,6 @@ class Knihovna_Ctenar_Block_Info extends Mage_Core_Block_Template {
    public function getCtenar(){
        /** @var $ctenar Knihovna_Ctenar_Model_Ctenar */
        $ctenar=Mage::getModel('core/session')->getLoggedUser();
-       $ctenar->setData('cislo_prukazu', $ctenar->getCisloprukazky());
        if (!$ctenar){
           return false;
 
