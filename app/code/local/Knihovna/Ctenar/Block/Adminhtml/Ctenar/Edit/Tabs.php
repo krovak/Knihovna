e9e2ddecfeac99402adb0cecf1a36cb272cc2b24
altpetr <alt.p@seznam.cz>
2014-02-27 11:05:42 +0100

    Revert "Pokus"
    This reverts commit 94819b93fd128e39d064d6b2ba95844d0a3be0b0.

--- app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tabs.php
+++ app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tabs.php
@@ -30,7 +30,7 @@ class Knihovna_Ctenar_Block_Adminhtml_Ctenar_Edit_Tabs extends Mage_Adminhtml_Bl
         $this->addTab('form_nepovinne', array(
             'label'   => Mage::helper('adminhtml')->__('Nepovinné údaje'),
             'title'   => Mage::helper('adminhtml')->__('Nepovinné údaje'),
-            'content' => $this->getLayout()->createBlock('ctenar/adminhtml_ctenar_edit_tab_epovinne')->toHtml()
+            'content' => $this->getLayout()->createBlock('ctenar/adminhtml_ctenar_edit_tab_nepovinne')->toHtml()
         ));
 
         return parent::_beforeToHtml();
