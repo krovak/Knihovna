4268fde4e08715ab43012de3cc4c3dee55ef1d0a
petralt <alt.p@seznam.cz>
2014-02-27 12:13:04 +0100

    Pokus

--- app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tabs.php
+++ app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tabs.php
@@ -35,8 +35,8 @@ class Knihovna_Ctenar_Block_Adminhtml_Ctenar_Edit_Tabs extends Mage_Adminhtml_Bl
 
 
         $this->addTab('form_nepovinne', array(
-            'label'   => Mage::helper('adminhtml')->__('Nepovinné údaje'),
-            'title'   => Mage::helper('adminhtml')->__('Nepovinné údaje'),
+            'label'   => Mage::helper('adminhtml')->__('Nepovinné údaje2'),
+            'title'   => Mage::helper('adminhtml')->__('Nepovinné údaje2'),
             'content' => $this->getLayout()->createBlock('ctenar/adminhtml_ctenar_edit_tab_nepovinne')->toHtml()
         ));
         return parent::_beforeToHtml();
