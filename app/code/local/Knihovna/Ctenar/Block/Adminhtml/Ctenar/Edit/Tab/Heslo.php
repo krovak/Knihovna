20ccf5fea3884a9f9854dc9b3cf41840b994a2e3
Petr <alt.p@seznam.cz>
2014-03-15 18:14:26 +0100

    Pokus.

--- app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Heslo.php
+++ app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Heslo.php
@@ -90,7 +90,7 @@ $ctenar->unsetData("heslo");
 
         }*/
 
-        echo '<form action="<?php $ctenar->pokus(\'123\',\'123\');?>" method="post">';
+        echo '<form action="$ctenar->pokus(\'123\',\'123\');" method="post">';
         echo '<input type="button" value="Run somePHPfile.php" />';
         echo '</form>';
 
