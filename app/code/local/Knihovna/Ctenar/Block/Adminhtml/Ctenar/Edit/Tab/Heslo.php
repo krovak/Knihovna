da016dbac58dfc3609b87a60a9e025e0ce1426b7
Petr <alt.p@seznam.cz>
2014-03-15 17:47:29 +0100

    Pokus.

--- app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Heslo.php
+++ app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Heslo.php
@@ -90,7 +90,7 @@ $ctenar->unsetData("heslo");
 
         }*/
 
-        echo '<form action="<?php echo "Ahoj!";?>" method="post">';
+        echo '<form action="<?php echo \'Ahoj!\';?>" method="post">';
         echo '<input type="button" value="Run somePHPfile.php" />';
         echo '</form>';
 
