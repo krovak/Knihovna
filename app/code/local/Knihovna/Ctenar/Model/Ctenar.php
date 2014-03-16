7858fa52909af8adf79873c05a7e048dbb0f0ccb
Petr <alt.p@seznam.cz>
2014-03-16 15:46:12 +0100

    Úprava e-mailu pro reset hesla.

--- app/code/local/Knihovna/Ctenar/Model/Ctenar.php
+++ app/code/local/Knihovna/Ctenar/Model/Ctenar.php
@@ -73,6 +73,9 @@ class Knihovna_Ctenar_Model_Ctenar extends Mage_Core_Model_Abstract
         $ctenar->setHeslo(sha1($noveHeslo));
         $ctenar->save();
         //echo $promenneProSablonu['heslo'];
+        $sablonaEmailu->setSenderName('NAME');
+        $sablonaEmailu->setSenderEmail('EMAIL@DOMAIN.com');
+
         $sablonaEmailu->setTemplateSubject('Vaše heslo bylo vyresetováno');
 
         $sablonaEmailu->send($email,'John Doe', $promenneProSablonu);
