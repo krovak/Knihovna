208faabf6eeea14f4bf8f1f87fa0a0ead15f21ac
zabaanto <antonin.zaba@centrum.cz>
2014-04-17 12:02:30 +0200

    export to csv

--- app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly.php
+++ app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly.php
@@ -8,6 +8,14 @@
  * email: info@iguru.eu
  */
 
+/*
+$defController = Mage::getBaseDir()
+    . DS . 'app' . DS . 'code' . DS . 'core'
+    . DS . 'Mage' . DS . 'Adminhtml' . DS . 'controllers'
+    . DS . 'Sales' . DS . 'OrderController.php';
+require_once $defController;*/
+
+
 class Knihovna_Tituly_Block_Adminhtml_Tituly extends Mage_Adminhtml_Block_Widget_Grid_Container {
 
     public function _construct(){
@@ -17,5 +25,15 @@ class Knihovna_Tituly_Block_Adminhtml_Tituly extends Mage_Adminhtml_Block_Widget
     }
     public function getHeaderText(){
         return 'Tituly';
+    }
+
+    /**
+     * Export order grid to CSVi format
+     */
+    public function exportCsvEnhancedAction()
+    {
+        $fileName   = 'knihy-' . gmdate('YmdHis') . '.csv';
+        $grid       = $this->getLayout()->createBlock('adminhtml/tituly_grid');
+        $this->_prepareDownloadResponse($fileName, $grid->getCsvFileEnhanced());
     }
 }
--- app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly/Grid.php
+++ app/code/local/Knihovna/Tituly/Block/Adminhtml/Tituly/Grid.php
@@ -35,6 +35,9 @@ class Knihovna_Tituly_Block_Adminhtml_Tituly_Grid extends Mage_Adminhtml_Block_W
 
     public function _prepareColumns()
     {
+        //$this->addExportType('*/*/exportCsvEnhanced', Mage::helper('tituly')->__('CSVe'));
+        //return parent::_prepareColumns();
+
         $this->addColumn('entity_id', array(
             'header' => 'ID',
             'index'  => 'entity_id',
@@ -67,12 +70,87 @@ class Knihovna_Tituly_Block_Adminhtml_Tituly_Grid extends Mage_Adminhtml_Block_W
             'filter_index'=>'nazev'
 
         ));
+
+        $this->addExportType('*/*/exportCsvEnhanced', Mage::helper('tituly')->__('CSVe'));
+        return parent::_prepareColumns();
         return $this;
     }
 
     public function getRowUrl($row)
     {
         return $this->getUrl('*/*/edit', array('id' => $row->getId()));
+    }
+
+    /// novy
+
+    protected $_exportPageSize = 500;
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getCsvFileEnhanced()
+    {
+        $this->_isExport = true;
+        $this->_prepareGrid();
+        $io = new Varien_Io_File();
+        $path = Mage::getBaseDir('var') . DS . 'export' . DS; //best would be to add exported path through config
+        $name = md5(microtime());
+        $file = $path . DS . $name . '.csv';
+        /**
+         * It is possible that you have name collision (summer/winter time +1/-1)
+         * Try to create unique name for exported .csv file
+         */
+        while (file_exists($file)) {
+            sleep(1);
+            $name = md5(microtime());
+            $file = $path . DS . $name . '.csv';
+        }
+        $io->setAllowCreateFolders(true);
+        $io->open(array('path' => $path));
+        $io->streamOpen($file, 'w+');
+        $io->streamLock(true);
+        $io->streamWriteCsv($this->_getExportHeaders());
+        //$this->_exportPageSize = load data from config
+        $this->_exportIterateCollectionEnhanced('_exportCsvItem', array($io));
+        if ($this->getCountTotals()) {
+            $io->streamWriteCsv($this->_getExportTotals());
+        }
+        $io->streamUnlock();
+        $io->streamClose();
+        return array(
+            'type'  => 'filename',
+            'value' => $file,
+            'rm'    => false // can delete file after use
+        );
+    }
+    public function _exportIterateCollectionEnhanced($callback, array $args)
+    {
+        $originalCollection = $this->getCollection();
+        $count = null;
+        $page  = 1;
+        $lPage = null;
+        $break = false;
+        $ourLastPage = 10;
+        while ($break !== true) {
+            $collection = clone $originalCollection;
+            $collection->setPageSize($this->_exportPageSize);
+            $collection->setCurPage($page);
+            $collection->load(/*true, true*/);
+            if (is_null($count)) {
+                $count = $collection->getSize();
+                $lPage = $collection->getLastPageNumber();
+            }
+            if ($lPage == $page || $ourLastPage == $page) {
+                $break = true;
+            }
+            $page ++;
+            foreach ($collection as $item) {
+                $item->setState($item->getState(), 'processing'); $item->save();
+                call_user_func_array(array($this, $callback), array_merge(array($item), $args));
+            }
+        }
+        /*exit();*/
     }
 
 }
