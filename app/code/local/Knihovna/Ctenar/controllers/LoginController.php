a9537ae3877b748322c17305d4efd3405f834412
Petr <alt.p@seznam.cz>
2014-04-22 01:12:30 +0200

    Revert.

--- app/code/local/Knihovna/Ctenar/controllers/LoginController.php
+++ app/code/local/Knihovna/Ctenar/controllers/LoginController.php
@@ -18,17 +18,12 @@ class Knihovna_Ctenar_LoginController extends Mage_Core_Controller_Front_Action
 
             $this->_redirect('*/index/vypujcky');
 
-
-        } else {
+            }
+         else {
             $this->loadLayout();
             $this->getLayout()->getBlock('content')->append($this->getLayout()->createBlock('ctenar/login'));
             $this->renderLayout();
-            $ctenar = Mage::getSingleton('core/session')->setUsername('MK00000001');
-            Mage::getSingleton('core/session')->setLoggedUser($ctenar);
-            $this->_redirect('*/index/vypujcky');
-            if (is_object($ctenar)){}
-            //$ctenar->resetHesla();
-        }
+         }
     }
     public function logoutAction()
     {
