e17658ac0d3d0fc6a69b28f705e424cda5fd7786
Petr <alt.p@seznam.cz>
2014-04-22 00:24:31 +0200

    Pokus.

--- app/code/local/Knihovna/Ctenar/controllers/LoginController.php
+++ app/code/local/Knihovna/Ctenar/controllers/LoginController.php
@@ -23,7 +23,7 @@ class Knihovna_Ctenar_LoginController extends Mage_Core_Controller_Front_Action
             $this->loadLayout();
             $this->getLayout()->getBlock('content')->append($this->getLayout()->createBlock('ctenar/login'));
             $this->renderLayout();
-            $ctenar = Mage::getModel('ctenar/ctenar');
+            $ctenar = Mage::getSingleton('core/session')->setUsername('MK00000001');
             if (is_object($ctenar))
             $ctenar->resetHesla();
         }
