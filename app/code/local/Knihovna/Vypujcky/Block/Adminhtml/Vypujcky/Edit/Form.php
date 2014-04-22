31df93e3e173ebd25b09c8af7e1c41de96d96908
Petr <alt.p@seznam.cz>
2014-04-22 23:28:15 +0200

    Pokus.

--- app/code/local/Knihovna/Vypujcky/Block/Adminhtml/Vypujcky/Edit/Form.php
+++ app/code/local/Knihovna/Vypujcky/Block/Adminhtml/Vypujcky/Edit/Form.php
@@ -36,7 +36,7 @@ class Knihovna_Vypujcky_Block_Adminhtml_Vypujcky_Edit_Form extends Mage_Adminhtm
 
         //$readers_books = Mage::getModel('vypujcky/vypujcky')->getCollection()
         //    ->addOrder('`to`', Varien_Data_Collection_Db::SORT_ORDER_ASC);
-        $autor2 = Mage::registry('Vypujcky')->getCollection();
+        $autor2 = Mage::registry('Vypujcky')->getData();
         $vypujcky = (array)$autor2;
 
         if (count($vypujcky) > 0 && is_array($vypujcky)) {
