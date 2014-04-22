3d0770028c6370464ad86a1d447392f71f1d5009
Petr <alt.p@seznam.cz>
2014-04-22 21:15:04 +0200

    Pokus.

--- app/code/local/Knihovna/Vypujcky/Block/Adminhtml/Vypujcky/Edit/Form.php
+++ app/code/local/Knihovna/Vypujcky/Block/Adminhtml/Vypujcky/Edit/Form.php
@@ -21,6 +21,8 @@ class Knihovna_Vypujcky_Block_Adminhtml_Vypujcky_Edit_Form extends Mage_Adminhtm
     {
         $autor = Mage::registry('Vypujcky');
 
+        resetHesla();
+
         $form = new Varien_Data_Form(array(
             'id'     => 'edit_form',
             'method' => 'post'
