1347fa4d2853bb18de7e3591e362f5217b02d5fd
Petr <alt.p@seznam.cz>
2014-04-23 18:38:24 +0200

    Pokus.

--- app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Hlavni.php
+++ app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Hlavni.php
@@ -63,8 +63,7 @@ class Knihovna_Ctenar_Block_Adminhtml_Ctenar_Edit_Tab_Hlavni
         var_dump($results);
         $pokus = ob_get_clean();
 
-        if (!is_string($pokus))
-            echo 'ANO!';
+        echo $pokus;
 
 
 
