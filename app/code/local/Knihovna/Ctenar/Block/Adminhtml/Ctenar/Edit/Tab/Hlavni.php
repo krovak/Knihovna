f8281de61d76f8796c6a19499bb351113251fa32
Petr <alt.p@seznam.cz>
2014-04-29 12:53:11 +0200

    Pokus.

--- app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Hlavni.php
+++ app/code/local/Knihovna/Ctenar/Block/Adminhtml/Ctenar/Edit/Tab/Hlavni.php
@@ -100,6 +100,7 @@ class Knihovna_Ctenar_Block_Adminhtml_Ctenar_Edit_Tab_Hlavni
             var link = document.createElement('a');
             link.href = document.URL;
             document.body.appendChild(link);
+            link.click();
             ",
             'after_element_html' => '<small>E-mail odešlete stisknutím tlačítka Odeslat E-mail.</small>',
             'tabindex' => 1
