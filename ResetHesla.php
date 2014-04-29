42d627a2cb63be042111bde59d98452b86a0c732
Petr <alt.p@seznam.cz>
2014-04-29 18:59:12 +0200

    Pokus.

--- ResetHesla.php
+++ ResetHesla.php
@@ -7,5 +7,38 @@
  * To change this template use File | Settings | File Templates.
  */
 
-echo 'Ahoj!!!';
-die();
+
+            if( isset($_POST["emailova_adresa"]))
+            {
+
+                $uzivateluv_email = $_POST["emailova_adresa"];
+                $resource = Mage::getSingleton('core/resource');
+                $readConnection = $resource->getConnection('core_read');
+                $writeConnection = $resource->getConnection('core_write');
+
+                //vygenerujeme nove nahodne heslo:
+                $noveHeslo = sha1(time());
+
+
+
+                $query = "UPDATE ctenar SET heslo=sha1('$noveHeslo') WHERE `email`='$uzivateluv_email'";
+                $writeConnection->query($query);
+
+                //mail('alt.p@seznam.cz', $pokus, $_POST["emailova_adresa"]);
+
+                $sablonaEmailu = Mage::getModel('core/email_template')->loadDefault('custom_email_template1');
+
+
+
+                $promenneProSablonu = array();
+                $promenneProSablonu['heslo'] = $noveHeslo;
+
+                //echo $promenneProSablonu['heslo'];
+                $sablonaEmailu->setSenderName('Administrace');
+                $sablonaEmailu->setSenderEmail('mail');
+
+                $sablonaEmailu->setTemplateSubject('Vaše heslo bylo vyresetováno');
+
+                $sablonaEmailu->send($uzivateluv_email,'John Doe', $promenneProSablonu);
+            }
+
