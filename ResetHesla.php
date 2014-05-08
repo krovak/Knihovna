1922d31d66a6bc818d7ced773c536cd45269eb30
Petr <alt.p@seznam.cz>
2014-05-08 03:41:11 +0200

    Pokus.

--- ResetHesla.php
+++ ResetHesla.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-echo'Ahoj';
+
             if (isset($_POST["emailova_adresa"]) and !empty($_POST["emailova_adresa"]))
             {
 
