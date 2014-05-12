481d91179199ddfd528f9ef1b61f173a9fd54e8e
Petr <alt.p@seznam.cz>
2014-05-12 19:17:47 +0200

    Pokus.

--- app/code/local/Knihovna/Ctenar/Model/Ctenar.php
+++ app/code/local/Knihovna/Ctenar/Model/Ctenar.php
@@ -152,8 +152,7 @@ class Knihovna_Ctenar_Model_Ctenar extends Mage_Core_Model_Abstract
             }
 
             }
-            var_dump($seznamCtenaru);
-        die();
+
         for ($i = 0; $i<count($seznamCtenaru); $i++)
 
         {
