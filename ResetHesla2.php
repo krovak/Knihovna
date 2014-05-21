1fc7aa780489f1862bb1e3fb779f90da490d73c7
Petr <alt.p@seznam.cz>
2014-05-21 21:58:36 +0200

    Pokus.

--- ResetHesla2.php
+++ ResetHesla2.php
@@ -56,3 +56,8 @@ foreach ($collection as $item){
 
 
 }
+echo '<script language="javascript">';
+echo 'alert("Vaše heslo bylo vyresetováno a na e-mail Vám bylo zasláno heslo nové. Nyní budete přesměrování
+na hlavní stránku knihovny")';
+echo 'window.location = "knihovna.cokoliv.eu"';
+echo '</script>';
