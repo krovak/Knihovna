9e2f64a21d8613cc1b1ed0318ef58e65946758d8
Petr <alt.p@seznam.cz>
2014-05-22 00:14:10 +0200

    Pokus.

--- app/code/local/Knihovna/Tituly/Block/Tituly.php
+++ app/code/local/Knihovna/Tituly/Block/Tituly.php
@@ -38,7 +38,7 @@ class Knihovna_Tituly_Block_Tituly extends Mage_Core_Block_Template{
 
 
         $books = Mage::getModel('tituly/tituly')->getCollection()
-            ->addFieldToFilter($podle, array("finset"=>$param));
+            ->addFieldToFilter($podle, array("finset"=>'%'.$param.'%'));
         echo '%'.$param.'%';
 
         echo $podle;
