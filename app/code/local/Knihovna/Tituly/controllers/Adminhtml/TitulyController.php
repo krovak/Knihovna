5a21decd69936cd565c5f1987e9a16780aa15eb1
zabaanto <antonin.zaba@centrum.cz>
2014-05-22 11:19:52 +0200

    import csv

--- app/code/local/Knihovna/Tituly/controllers/Adminhtml/TitulyController.php
+++ app/code/local/Knihovna/Tituly/controllers/Adminhtml/TitulyController.php
@@ -85,7 +85,7 @@ class Knihovna_Tituly_Adminhtml_TitulyController extends Mage_Adminhtml_Controll
 
         $target = Mage::getModel('knihovna/tituly');
         $target->setautor('ja');
-        $target->setisbn('ISBN 80-204-0105-9');
+        $target->setisbn('ISBN 80-204-0105-8');
         $target->setpocet_stranek('90');
         $target->setrok_vydan('2013');
         $target->setzanrt('2');
