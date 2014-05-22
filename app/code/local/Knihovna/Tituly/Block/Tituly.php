b9391d16c987201c8b229e89847c7bc7af7f8f9c
Petr <alt.p@seznam.cz>
2014-05-22 09:10:51 +0200

    Pokus.

--- app/code/local/Knihovna/Tituly/Block/Tituly.php
+++ app/code/local/Knihovna/Tituly/Block/Tituly.php
@@ -52,7 +52,7 @@ class Knihovna_Tituly_Block_Tituly extends Mage_Core_Block_Template{
          * Execute the query and store the results in $results
          */
         $results = $readConnection->fetchAll($query);
-        echo '$results[$1]["entity_id"]';
+        echo $results[1]["entity_id"];
         //echo '<pre>'; print_r($results); echo '</pre>';
 
         $books = Mage::getModel('tituly/tituly')->getCollection();
