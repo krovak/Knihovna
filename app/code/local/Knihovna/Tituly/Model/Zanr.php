28e9b2ff43e6312819564fe6e4fae47617f669a6
zabaanto <antonin.zaba@centrum.cz>
2014-06-05 14:07:17 +0200

    import csv

--- app/code/local/Knihovna/Tituly/Model/Zanr.php
+++ app/code/local/Knihovna/Tituly/Model/Zanr.php
@@ -8,4 +8,22 @@ class Knihovna_Tituly_Model_Zanr extends Mage_Core_Model_Abstract
         $this->_init('tituly/zanr');
     }
 
+    public function getIDbyZanr($zanr) {
+        $db = Mage::getModel('tituly/zanr')
+            ->getCollection()
+            ->addFieldToSelect('entity_id')
+            ->addFieldToFilter('zanr', array('eq' => $zanr))
+            ->getLastItem();
+
+        $data = $db->getData();
+        if (@$data['entity_id']) {
+            return $data['entity_id'];
+        } else {
+            $new_zanr = Mage::getModel('tituly/zanr');
+            $new_zanr->setData('zanr',$zanr);
+            $new_zanr->save();
+            getIdByName($zanr);
+        }
+    }
+
 }
