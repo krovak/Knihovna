af40ae2d4feea6cdf88db0b3446539f0922d1796
zabaanto <antonin.zaba@centrum.cz>
2014-06-05 13:19:59 +0200

    import csv

--- app/code/local/Knihovna/Tituly/controllers/Adminhtml/TitulyController.php
+++ app/code/local/Knihovna/Tituly/controllers/Adminhtml/TitulyController.php
@@ -91,7 +91,7 @@ class Knihovna_Tituly_Adminhtml_TitulyController extends Mage_Adminhtml_Controll
             // potreba zjistit jestli uz neexistuje v databazi
             // rozsekat na jmeno (jmen muze byt vic) a prijmeni (prijmeni pouze jedno..a to to posledni)
                 $regularExpression = "\S"; // rozsekame podle bilych znaku
-                $autorArray = preg_split($regularExpression,$radek[0]); // tady jsou vsechna jmena + prijmeni autora
+                $autorArray = preg_split($regularExpression,"ahojkz jak se"); // tady jsou vsechna jmena + prijmeni autora
             //sloucime jmena s mezerami do jednoho
                 $jmeno = '';
                 for($i = 0; i < end($autorArray)-1; $i++)
