25abbec92e0a505826d54e3cdbcf94a97802f051
zabaanto <antonin.zaba@centrum.cz>
2014-06-05 13:23:21 +0200

    import csv

--- app/code/local/Knihovna/Tituly/controllers/Adminhtml/TitulyController.php
+++ app/code/local/Knihovna/Tituly/controllers/Adminhtml/TitulyController.php
@@ -92,6 +92,7 @@ class Knihovna_Tituly_Adminhtml_TitulyController extends Mage_Adminhtml_Controll
             // rozsekat na jmeno (jmen muze byt vic) a prijmeni (prijmeni pouze jedno..a to to posledni)
                 $regularExpression = "/\S/"; // rozsekame podle bilych znaku
                 $autorArray = preg_split($regularExpression,$radek[0]); // tady jsou vsechna jmena + prijmeni autora
+                var_dump($autorArray);
             //sloucime jmena s mezerami do jednoho
                 $jmeno = '';
                 for($i = 0; $i < end($autorArray)-1; $i++)
