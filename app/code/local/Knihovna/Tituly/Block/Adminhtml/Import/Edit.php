45e5222fdf9b4f1250a8eac0c99688810930fd9a
zabaanto <antonin.zaba@centrum.cz>
2014-06-16 11:47:31 +0200

    import from csv

--- app/code/local/Knihovna/Tituly/Block/Adminhtml/Import/Edit.php
+++ app/code/local/Knihovna/Tituly/Block/Adminhtml/Import/Edit.php
@@ -4,7 +4,7 @@
  * Date: 20.12.12
  * Time: 13:37
  */
-class Knihovna_Tituly_Block_Adminhtml_Import_Edit extends Mage_Adminhtml_Block_Widget_Grid_Container
+class Knihovna_Tituly_Block_Adminhtml_Import_Edit extends Mage_Adminhtml_Block_Widget_Form_Container
 {
     public function _construct()
     {
