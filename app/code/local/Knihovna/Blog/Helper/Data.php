091ea3b2b91809a06f17b02ab57993ea29d16f15
zidar9 <zidar9@seznam.cz>
2015-02-26 13:57:57 +0100

    // příklad v EAV

---
+++ app/code/local/Knihovna/Blog/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Knihovna_Blog_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/Knihovna/Blog/Model/Post.php
@@ -0,0 +1,9 @@
+<?php
+
+class Knihovna_Blog_Model_Post extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('knihovna_blog/post');
+    }
+}
